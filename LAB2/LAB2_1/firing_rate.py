d7f311bfbec87649546f30056664d6ccff238eb1
Marco Pampaloni <crybot@hotmail.it>
2023-05-11 14:41:57 +0200

    Add assignment LAB2_1

---
+++ LAB2/LAB2_1/firing_rate.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+class FiringRate():
+    def __init__(self, input_neurons, output_neurons):
+        self.input_neurons = input_neurons
+        self.weights = np.random.random((output_neurons, input_neurons))
+
+    def __call__(self, *args, **kwargs):
+        return self.forward(*args, **kwargs)
+
+    def forward(self, x):
+        return np.dot(self.weights, x.T)
+    
+    def update(self, w):
+        self.weights = w.copy()
+
+    def backward(self):
+        pass
+
+class HebbFiringRate(FiringRate):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def forward(self, x):
+        self.last_x = x
+        self.last_y = super().forward(x)
+        return self.last_y
+
+    def backward(self, eta = 1e-3):
+        w_new = self.weights + eta * self.last_y * self.last_x
+        self.update(w_new)
+
+class OjaFiringRate(HebbFiringRate):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def backward(self, eta = 1e-3, alpha = 1e-1):
+        w_new = self.weights + eta * \
+                (self.last_y*self.last_x - alpha*self.last_y**2 * self.weights)
+        self.update(w_new)
+
+class SubtractiveNormFiringRate(HebbFiringRate):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def backward(self, eta = 1e-3, alpha = 1):
+        w_new = self.weights + eta * \
+                (self.last_y * self.last_x - \
+                (self.last_y * self.last_x.sum()).sum()/self.input_neurons)
+        self.update(w_new)
+
+class BCMFiringRate(HebbFiringRate):
+    def __init__(self, *args, theta0 = 1e-5, tau = 1e-7):
+        super().__init__(*args)
+        self.theta = theta0
+        self.tau = tau
+
+    def forward(self, *args, **kwargs):
+        super().forward(*args, **kwargs)
+        self.theta = self.theta + self.tau * (np.asscalar(self.last_y**2) - self.theta)
+        # print(f"theta = {self.theta}, y = {self.last_y}")
+
+    def backward(self, eta = 1e-3):
+        w_new = self.weights + eta * (self.last_y*self.last_x - (self.last_y - self.theta))
+        self.update(w_new)
+
+
---
+++ LAB2/LAB2_1/hebb.py
@@ -0,0 +1,85 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import yaml
+import glob
+import os.path
+import random
+from firing_rate import FiringRate, HebbFiringRate, OjaFiringRate, SubtractiveNormFiringRate, BCMFiringRate
+from operator import itemgetter
+
+def correlation(X):
+    return np.dot(X.T, X)
+
+def training(model, x, eta = 1e-3, epochs = 100, eps=1e-5, plot = True):
+    bcm = isinstance(model, BCMFiringRate)
+    w_new = model.weights
+    w_history = [w_new]
+    theta_history = [model.theta] if bcm else []
+    
+    for epoch in range(epochs):
+        np.random.shuffle(x)
+        w_old = w_new
+        for xi in x:
+            y = model(np.array([xi]))
+            model.backward(eta)
+        w_new = model.weights
+        w_history.append(w_new)
+        if bcm:
+            theta_history.append(model.theta)
+
+        if np.linalg.norm(w_new - w_old) <= eps:
+            break;
+
+    corr = correlation(x)
+    e_val, e_vec = np.linalg.eig(corr)
+    e_vec = e_vec.T
+    if plot:
+        ws = w_history
+
+        fig, axs = plt.subplots(3, 2)
+        fig.tight_layout()
+
+        axs[0, 0].scatter(x[:, 0], x[:, 1])
+        axs[0, 0].quiver(*model.weights.T, scale=5, color='tab:red', width=0.009)
+        axs[0, 0].quiver(*e_vec[0], scale=5, color='tab:orange', width=0.013)
+
+        axs[0, 1].plot(range(len(ws)), list(map(np.linalg.norm, ws)), color='tab:orange')
+        axs[0, 1].set_title('W norm over epochs')
+        axs[1, 0].plot(range(len(ws)), list(map(lambda w: w[:, 0], ws)), color='tab:orange')
+        axs[1, 0].set_title('W_0 over epochs')
+        axs[1, 1].plot(range(len(ws)), list(map(lambda w: w[:, 1], ws)), color='tab:orange')
+        axs[1, 1].set_title('W_1 over epochs')
+        axs[2, 0].plot(range(len(ws)), list(map(lambda w: w.sum(), ws)), color='tab:orange')
+        axs[2, 0].set_title('W sum over epochs')
+
+        if bcm:
+            axs[2, 1].plot(range(len(theta_history)), theta_history, color='tab:orange')
+            axs[2, 1].set_title('Theta (BCM) over epochs')
+        plt.show()
+
+    return w_history, corr, e_val, e_vec
+
+def main():
+    np.random.seed(42)
+    random.seed(42)
+
+    DATASET_PATH = './lab2_1_data.csv'
+    ds = pd.read_csv(DATASET_PATH, header=None)
+    ds = ds.transpose()
+
+    # model = HebbFiringRate(2, 1)
+    # ws, _, _, _ = training(model, ds.values)
+
+    # model = OjaFiringRate(2, 1)
+    # ws, _, _, _ = training(model, ds.values)
+
+    # model = SubtractiveNormFiringRate(2, 1)
+    # ws, _, _, _ = training(model, ds.values)
+
+    model = BCMFiringRate(2, 1, theta0 = 0.1, tau = 0.1)
+    ws, _, _, _ = training(model, ds.values, epochs = 10000, eta = 0.1)
+
+if __name__ == '__main__':
+    main()
+
